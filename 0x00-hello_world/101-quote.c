f440b23eda9c1b7e7735077c6bf06212efe7d83d
GodfreyNjo <kahunyurogodfrey@gmail.com>
2023-02-16 18:28:03 -0800

    c8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,17 @@
-#include <unistd.h>
+#include <unistd.h>  // for write() and STDERR_FILENO
+#include <string.h>  // for strlen()
 
 /**
- * This program prints a message to standard error.
+ * Writes an error message to standard error.
  *
- * The message printed is "and that piece of art is useful" - Dora Korpar, 2015-10-19\n".
+ * This program writes the following error message to standard error:
+ *   "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
  *
- * @return 1 to indicate an error has occurred.
+ * The message is terminated by a newline character (\n).
  */
 int main() {
-// Define the message to be printed.
 char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    
-// Determine the length of the message.
-size_t len = sizeof(msg) - 1;
-    
-// Write the message to standard error.
+size_t len = strlen(msg);
 write(STDERR_FILENO, msg, len);
-    
-// Return an error code.
 return (1);
 }
