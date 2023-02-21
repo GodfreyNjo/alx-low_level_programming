9ba9b4b8b3372d93080727e4477d5b3587680750
GodfreyNjo <kahunyurogodfrey@gmail.com>
2023-02-21 09:37:04 -0800

    c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,13 @@
  * Return: 0
  */
 int print_putchar(void)
-	{
-    	char c[] = "_putchar\n";
-    	int i;
+{
+    char c[] = "_putchar\n";
+    int i;
 
-    	for (i = 0; i < 9; i++)
-        	_putchar(c[i]);
+    for (i = 0; i < 9; i++)
+        _putchar(c[i]);
 
-    	return (0);
-	}
+    return (0);
+}
 
