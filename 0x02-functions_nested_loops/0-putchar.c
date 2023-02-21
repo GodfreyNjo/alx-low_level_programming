7725824fb0286d43530c4c48b761a11f1caeb4a9
GodfreyNjo <kahunyurogodfrey@gmail.com>
2023-02-21 09:25:22 -0800

    c1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_putchar - prints _putchar followed by a newline
+ *
+ * Return: 0
+ */
+int print_putchar(void)
+	{
+    	char c[] = "_putchar\n";
+    	int i;
+
+    	for (i = 0; i < 9; i++)
+        	_putchar(c[i]);
+
+    	return (0);
+	}
+
