0c4b38639e78182f939e9202a26f5853372a9b28
GodfreyNjo <kahunyurogodfrey@gmail.com>
2023-02-22 16:11:38 -0800

    c8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day for Jack Bauer.
+ * from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			printf("%02d:%2d\n", h, m);
+		}
+	}
+}
