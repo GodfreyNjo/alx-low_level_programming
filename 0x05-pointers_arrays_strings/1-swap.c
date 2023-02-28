deeb51090a35c6a2fca0842b1aecf08a515c08a1
GodfreyNjo <kahunyurogodfrey@gmail.com>
2023-02-27 16:05:54 -0800

    edit0

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,21 +4,12 @@
  * @a: integer to swap
  * @b: integer to swap
  */
-
 void swap_int(int *a, int *b)
 {
-	int temp = *a;
+	int m;
+
+	m = *a;
 	*a = *b;
-	*b = temp;
-}
-
-int main(void)
-{
-	int x = 10, y = 20;
-
-	printf("Before swap: x = %d, y = %d\n", x, y);
-	swap_int(&x, &y);
-	printf("After swap: x = %d, y = %d\n", x, y);
-	return (0);
+	*b = m;
 }
 
