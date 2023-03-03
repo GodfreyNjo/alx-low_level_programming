e90e0eb34830ea8df7ab9e94ae97995e74682d92
GodfreyNjo <kahunyurogodfrey@gmail.com>
2023-03-02 18:37:38 -0800

    edit n10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
- * no main.h here
- * advance
- */
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
@@ -12,18 +13,12 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
-*(p + 5) = 98;
-/* ...so that this prints 98\n */
+
+/* Update the value of the third element of the array to be 98 */
+*(a + 2) = 98;
+
+/* Print the value of a[2] */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
-
 
